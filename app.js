cf930ad5aa50e879ec95fc9036677a5a9f116468
fenil29 <fenilkaneria99@gmail.com>
2020-12-12 14:27:08 +0530

    bug fix in delete user

--- app.js
+++ app.js
@@ -1656,16 +1656,17 @@ app.put("/api/employee/:id", verifyHR, (req, res) => {
   });
 });
 
-app.delete("/api/Employee/:id", verifyHR, (req, res) => {
-  Employee.findByIdAndRemove({ _id: req.params.id }, function (err, employee) {
-    if (!err) {
-      console.log(" state deleted");
-      res.send(employee);
-    } else {
-      console.log(err);
-      res.send("error");
-    }
-  });
+app.delete("/api/employee/:id", verifyHR, (req, res) => {
+  // Employee.findByIdAndRemove({ _id: req.params.id }, function (err, employee) {
+  //   if (!err) {
+  //     console.log(" state deleted");
+  //     res.send(employee);
+  //   } else {
+  //     console.log(err);
+  //     res.send("error");
+  //   }
+  // });
+  res.send("error");
   console.log("delete");
   console.log(req.params.id);
 });
